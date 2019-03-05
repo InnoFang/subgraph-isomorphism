d855152e4ae4c1354d46ffd2db8b7a485e63b564
InnoFang <innofang@outlook.com>
2019-03-05 10:39:09 +0800

    :tada: init project

---
+++ src/test/java/io/github/innofang/algorithms/UllmannTest.java
@@ -0,0 +1,17 @@
+package io.github.innofang.algorithms;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class UllmannTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
