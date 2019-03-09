1cd0dc7c0541277a08212728223ca2fb9902a6ce
InnoFang <innofang@outlook.com>
2019-03-09 09:50:42 +0800

    :art: clean

--- src/main/java/io/github/innofang/graph/GraphReader.java
+++ src/main/java/io/github/innofang/graph/GraphReader.java
@@ -6,28 +6,6 @@ import io.github.innofang.graph.datasets.DataSetStrategy;
 import java.io.*;
 import java.util.List;
 
-/**
- *
- * graph Data Template:
- * ================
- * t # n    // nth graph
- * v i j    // the i is the vertex number, and the j is the vertex label
- * e i j k  // edge <i, j> which label is k
- *
- * graph Data Example:
- * ================
- * t # 1
- * v 0 2
- * v 1 2
- * v 2 6
- * v 3 6
- * e 0 1 2
- * e 0 2 2
- * e 1 3 2
- * e 2 4 3
- *
- */
-
 public class GraphReader {
 
     private DataSetStrategy dataSetStrategy;
