86a0b7ed4801a45f5cd903a5690ce466f0679275
InnoFang <innofang@outlook.com>
2019-03-11 08:28:52 +0800

    :sparkles: create a Pair

---
+++ src/main/java/io/github/innofang/utils/Pair.java
@@ -0,0 +1,41 @@
+package io.github.innofang.utils;
+
+import javafx.beans.NamedArg;
+
+public class Pair<K,V>  {
+
+    private K key;
+
+    private V value;
+
+    public K getKey() { return key; }
+
+    public V getValue() { return value; }
+
+    public Pair(@NamedArg("key") K key, @NamedArg("value") V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode() * 13 + (value == null ? 0 : value.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof Pair) {
+            Pair pair = (Pair) o;
+            if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+            if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+            return true;
+        }
+        return false;
+    }
+}
