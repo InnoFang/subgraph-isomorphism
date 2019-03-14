20f8dc2b3512028f1e8907e95aa8805718463e0d
InnoFang <innofang@outlook.com>
2019-03-14 18:20:48 +0800

    :white_check_mark: test VF2 with easy data

--- src/test/java/io/github/innofang/algorithm/impl/VF2Test.java
+++ src/test/java/io/github/innofang/algorithm/impl/VF2Test.java
@@ -9,10 +9,28 @@ import java.io.IOException;
 public class VF2Test {
 
     @Test
-    public void testVF2() throws IOException {
+    public void testVF2WithBigData() throws IOException {
 
         String queryGraphPath = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\graphDB\\Q4.my";
         String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\graphDB\\mygraphdb.data";
+        VF2 vf2 = new VF2();
+
+        TestHelper.testIsomorphismAlgorithm(
+                targetGraphPath,
+                queryGraphPath,
+                vf2,
+                new NormalDataSet(),
+                true,
+                (algorithm, queryGraphIndex, targetGraphIndex) ->
+                        System.out.printf("t # %d is isomorphic T # %d\n", queryGraphIndex, targetGraphIndex)
+        );
+    }
+
+    @Test
+    public void testVF2() throws IOException {
+
+        String queryGraphPath = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\data\\query_graph.txt";
+        String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\data\\target_graph.txt";
         VF2 vf2 = new VF2();
 
         TestHelper.testIsomorphismAlgorithm(
