9dcec9adc6b742c915ed339d966fb763a1e33aa3
InnoFang <innofang@outlook.com>
2019-03-15 11:23:41 +0800

    :art: update

--- src/main/java/io/github/innofang/algorithm/impl/SPath.java
+++ src/main/java/io/github/innofang/algorithm/impl/SPath.java
@@ -1,4 +1,30 @@
 package io.github.innofang.algorithm.impl;
 
-public class SPath {
+import io.github.innofang.algorithm.IsomorphismAlgorithm;
+import io.github.innofang.graph.bean.Graph;
+
+import java.util.HashMap;
+
+public class SPath implements IsomorphismAlgorithm {
+
+    private Graph targetGraph;
+    private Graph queryGraph;
+
+    private void initialize(Graph targetGraph, Graph queryGraph) {
+        this.targetGraph = targetGraph;
+        this.queryGraph = queryGraph;
+    }
+
+    @Override
+    public boolean isSubGraphIsomorphism(Graph targetGraph, Graph queryGraph) {
+        initialize(targetGraph, queryGraph);
+        return false;
+    }
+
+
+
+    @Override
+    public HashMap<String, String> getMapping() {
+        return null;
+    }
 }
