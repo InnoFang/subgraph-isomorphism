0d330b6de2e6f0586919d98c066036886f4b3f35
InnoFang <innofang@outlook.com>
2019-03-18 11:38:50 +0800

    :white_check_mark: test GraphReader

--- src/test/java/io/github/innofang/graph/GraphReaderTest.java
+++ src/test/java/io/github/innofang/graph/GraphReaderTest.java
@@ -1,6 +1,7 @@
 package io.github.innofang.graph;
 
 import io.github.innofang.graph.bean.Graph;
+import io.github.innofang.graph.datasets.CAAstroPhDataSet;
 import io.github.innofang.graph.datasets.NormalDataSet;
 import org.junit.Test;
 
@@ -10,12 +11,23 @@ import java.util.List;
 public class GraphReaderTest {
 
     @Test
-    public void testRead() throws IOException {
+    public void testNormalDataSet() throws IOException {
         String path = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\graphDB\\Q4.my";
         GraphReader reader = new GraphReader();
         reader.setDataSetStrategy(new NormalDataSet());
         List<Graph> graphList = reader.read(path);
         for (Graph graph: graphList) {
+            System.out.println(graph);
+        }
+    }
+
+    @Test
+    public void testCAAstroPhDataSet() throws IOException {
+        String path = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\ca-AstroPh\\CA-AstroPh.txt";
+        GraphReader reader = new GraphReader();
+        reader.setDataSetStrategy(new CAAstroPhDataSet());
+        List<Graph> graphList = reader.read(path);
+        for (Graph graph : graphList) {
             System.out.println(graph);
         }
     }
