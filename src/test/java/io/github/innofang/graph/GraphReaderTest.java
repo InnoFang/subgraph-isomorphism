3f5353772f21d4a5b1f18d491e9807a22d4c24f1
InnoFang <innofang@outlook.com>
2019-03-18 11:52:14 +0800

    :white_check_mark: test GraphReader

--- src/test/java/io/github/innofang/graph/GraphReaderTest.java
+++ src/test/java/io/github/innofang/graph/GraphReaderTest.java
@@ -3,6 +3,7 @@ package io.github.innofang.graph;
 import io.github.innofang.graph.bean.Graph;
 import io.github.innofang.graph.datasets.CAAstroPhDataSet;
 import io.github.innofang.graph.datasets.NormalDataSet;
+import io.github.innofang.graph.datasets.RoadNetCADataSet;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,6 +25,8 @@ public class GraphReaderTest {
 
     @Test
     public void testCAAstroPhDataSet() throws IOException {
+        // Nodes: 18772 Edges: 396160
+
         String path = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\ca-AstroPh\\CA-AstroPh.txt";
         GraphReader reader = new GraphReader();
         reader.setDataSetStrategy(new CAAstroPhDataSet());
@@ -35,4 +38,18 @@ public class GraphReaderTest {
         Assert.assertEquals(ca_astro_ph.getEdgeList().size(), 396160);
     }
 
+    @Test
+    public void testRoadNetCA() throws IOException {
+        // Nodes: 1965206 Edges: 5533214
+
+        String path = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\roadNet-CA\\roadNet-CA.txt";
+        GraphReader reader = new GraphReader();
+        reader.setDataSetStrategy(new RoadNetCADataSet());
+        List<Graph> graphList = reader.read(path);
+
+        Graph ca_astro_ph = graphList.get(0);
+
+        Assert.assertEquals(ca_astro_ph.getVertexList().size(), 1965206);
+        Assert.assertEquals(ca_astro_ph.getEdgeList().size(), 5533214);
+    }
 }
