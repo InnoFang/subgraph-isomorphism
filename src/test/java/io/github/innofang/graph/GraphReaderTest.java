d1221333f649191fb877d3f833b81bfe330fae84
InnoFang <innofang@outlook.com>
2019-03-29 08:39:03 +0800

    :bug: fix some bugs

--- src/test/java/io/github/innofang/graph/GraphReaderTest.java
+++ src/test/java/io/github/innofang/graph/GraphReaderTest.java
@@ -14,13 +14,29 @@ public class GraphReaderTest {
 
     @Test
     public void testNormalDataSet() throws IOException {
-        String path = "F:\\IDEA\\subgraph-isomorphism\\src\\main\\resources\\graphDB\\Q4.my";
+        String path = "F:\\IDEA\\subgraph-isomorphism\\src\\test\\resources\\graphDB\\Q4.my";
         GraphReader reader = new GraphReader();
         reader.setDataSetStrategy(new NormalDataSet());
         List<Graph> graphList = reader.read(path);
         for (Graph graph: graphList) {
             System.out.println(graph);
         }
+    }
+
+    @Test
+    public void testGraphDB() throws IOException {
+        String path = "F:\\IDEA\\subgraph-isomorphism\\src\\test\\resources\\graphDB\\mygraphdb.data";
+        GraphReader reader = new GraphReader();
+        reader.setDataSetStrategy(new NormalDataSet());
+        List<Graph> graphList = reader.read(path);
+        int vertexSum = 0;
+        int edgeSum = 0;
+        for (Graph graph: graphList) {
+            vertexSum += graph.getVertexList().size();
+            edgeSum += graph.getEdgeList().size();
+        }
+        System.out.println("The number of vertex (AVR): " + (vertexSum / 10000.0)); // 24.8075
+        System.out.println("The number of edge (AVR): " + (edgeSum / 10000.0));     // 26.8058
     }
 
     @Test
