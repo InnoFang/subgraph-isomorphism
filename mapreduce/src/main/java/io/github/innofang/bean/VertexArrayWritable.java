69bd3faf58e29b7e04fe524139fbcd19ecbef2ca
InnoFang <innofang@outlook.com>
2019-04-09 19:43:14 +0800

    :fire: clean

--- mapreduce/src/main/java/io/github/innofang/bean/VertexArrayWritable.java
+++
@@ -1,9 +0,0 @@
-package io.github.innofang.bean;
-
-import org.apache.hadoop.io.ArrayWritable;
-
-public class VertexArrayWritable extends ArrayWritable {
-    public VertexArrayWritable() {
-        super(Vertex.class);
-    }
-}
