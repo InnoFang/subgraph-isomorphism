ff9e17d94c3f8e04b387b03c84f9b336dfafed9f
InnoFang <innofang@outlook.com>
2019-04-10 11:17:17 +0800

    :art: update and optimize

--- src/main/java/io/github/innofang/graph/datasets/NormalDataSet.java
+++ src/main/java/io/github/innofang/graph/datasets/NormalDataSet.java
@@ -7,18 +7,18 @@ import io.github.innofang.graph.exceptions.GraphDataErrorException;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 
 /**
- *
  * Data File Format
  * "t # N" means the Nth graph,
  * "v M L" means that the Mth vertex in this graph has label L,
  * "e P Q L" means that there is an edge connecting the Pth vertex with the Qth vertex. The edge has label L.
- *
+ * <p>
  * Each test file or query file end with "t # -1".
- *
+ * <p>
  * graph Data Example:
  * ================
  * t # 0
@@ -30,7 +30,7 @@ import java.util.List;
  * e 0 2 2
  * e 1 3 2
  * e 2 4 3
- *
+ * <p>
  * t # -1
  */
 
@@ -44,28 +44,44 @@ public class NormalDataSet implements DataSetStrategy {
 
         List<Graph> graphList = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-        Graph graph = null;
         String line;
         int lineNumber = 0;
+        HashSet<Vertex> vertices = new HashSet<>();
+        HashSet<Edge> edges = new HashSet<>();
+        int oldGraphId = 0;
+        boolean first = true;
         while ((line = reader.readLine()) != null) {
-            ++ lineNumber;
+            ++lineNumber;
             line = line.trim();
             if (line.equals("")) {
                 continue;
             }
-            String[] info = line.split(" ");
-            if (info[0].equals("t")) {
-                if (graph != null) {
+            String[] info = line.split("\\s+");
+            if (info[0].equals("t") && info.length == 3) {
+                int newGraphId = Integer.valueOf(info[2]);
+                if (newGraphId == -1) {
+                    Graph graph = new Graph(vertices, edges);
+                    graph.setGraphId(oldGraphId);
+                    vertices.clear();
+                    edges.clear();
                     graphList.add(graph);
+                    break;
                 }
-                graph = new Graph();
-            }
-            else if (info[0].equals("v") && info.length == 3) {
-                Vertex vertex = new Vertex(info[1], info[2]);
-                graph.addVertex(vertex);
+                if (first) {
+                    oldGraphId = newGraphId;
+                    first = false;
+                    continue;
+                }
+                Graph graph = new Graph(vertices, edges);
+                graph.setGraphId(oldGraphId);
+                vertices.clear();
+                edges.clear();
+                graphList.add(graph);
+                oldGraphId = newGraphId;
+            } else if (info[0].equals("v") && info.length == 3) {
+                vertices.add(new Vertex(Integer.parseInt(info[1]), info[2]));
             } else if (info[0].equals("e") && info.length == 4) {
-                Edge edge = new Edge(info[1], info[2], info[3]);
-                graph.addEdge(edge);
+                edges.add(new Edge(Integer.parseInt(info[1]), Integer.parseInt(info[2]), info[3]));
             } else {
                 throw new GraphDataErrorException(
                         String.format("graph test isn't standard, " +
