bb183f89016fe4f056d777fb367e8e085951cf89
InnoFang <innofang@outlook.com>
2019-04-14 09:58:36 +0800

    :sparkles: Use Matcher

---
+++ mapreduce/src/main/java/io/github/innofang/lib/Matcher.java
@@ -0,0 +1,45 @@
+package io.github.innofang.lib;
+
+import io.github.innofang.bean.Graph;
+import io.github.innofang.bean.Pair;
+import io.github.innofang.mapper.vf2.VF2Mapper;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Matcher {
+
+    public static boolean match(State state, Mapper.Context context, Graph graph, Visitor visitor) throws IOException, InterruptedException {
+        if (state.isSuccess()) {
+            return visitor.visit(context, graph, state.getMapping());
+        }
+
+        if (state.isFailure()) {
+            return false;
+        }
+
+        State.PairIterator iterator = state.iterator();
+        while (iterator.hasNextPair()) {
+            Pair<Integer, Integer> pair = iterator.nextPair();
+            if (state.isFeasiblePair(pair)) {
+                State s = state.clone();
+                state.addPair(pair);
+                if (match(s, context, graph, visitor)) {
+                    s.backTrack(pair);
+                    s = null;
+                    return true;
+                } else {
+                    s.backTrack(pair);
+                    s = null;
+                }
+            }
+        }
+        return false;
+    }
+
+    public interface Visitor {
+        boolean visit(Mapper.Context context, Graph graph, HashMap<Integer, Integer> mapping) throws IOException, InterruptedException;
+    }
+}
