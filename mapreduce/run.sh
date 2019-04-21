95df2edfa74a00b87502e5cacbd9c8d10ef41fa5
InnoFang <innofang@outlook.com>
2019-04-21 11:24:43 +0800

    :art: update run.sh

--- mapreduce/run.sh
+++ mapreduce/run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo '=== Step one: Generate the jar file ==='
 mvn clean package -DskipTests
 echo '\n'
@@ -7,4 +8,7 @@ scp target/mapreduce-1.0.jar innofang@localhost:~/lib
 echo '\n'
 
 echo '=== Step three: Run the specify class/program ==='
-hadoop jar /home/innofang/lib/mapreduce-1.0.jar io.github.innofang.UllmannDriver hdfs://localhost:9000/resources/graphDB/Q4.my hdfs://localhost:9000/output/subgraph-isomorphism/ullmann
+hadoop jar /home/innofang/lib/mapreduce-1.0.jar io.github.innofang.VF2Driver \
+-s hdfs:////localhost:9000/datasets/ermil-Eu-core/email-Eu-core-department-labels.txt \
+-t hdfs:////localhost:9000/datasets/emil-Eu-core/Q4-10-unweighted.my \
+-o hdfs:////localhost:9000/output/subgraph-isomorphism/vf2
