b2c3c3c58758a70fce5d6fd520f16d3eccb95404
InnoFang <innofang@outlook.com>
2019-06-02 13:14:13 +0800

    :white_check_mark: test VF2

--- src/test/java/io/github/innofang/lib/VF2Test.java
+++ src/test/java/io/github/innofang/lib/VF2Test.java
@@ -4,7 +4,7 @@ import io.github.innofang.graph.datasets.EmailEuCoreDataSet;
 import io.github.innofang.graph.datasets.GraphDBDataSet;
 import io.github.innofang.graph.datasets.UnweightedDiGraphDataSet;
 import io.github.innofang.graph.datasets.UnweightedGraphDBDataSet;
-import io.github.innofang.util.TestHelper;
+import io.github.innofang.util.MatchHelper;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,10 +14,10 @@ public class VF2Test {
 
     @Test
     public void testVF2WithBigData() throws IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        String queryGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\graphDB\\Q4-100.my";
-        String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\graphDB\\mygraphdb.data";
+        String queryGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\graphDB\\Q4.txt";
+        String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\graphDB\\dataset.txt";
 
-        TestHelper.testIsomorphismAlgorithm(
+        MatchHelper.testIsomorphismAlgorithm(
                 targetGraphPath,
                 queryGraphPath,
                 VF2State.class,
@@ -30,11 +30,25 @@ public class VF2Test {
     }
 
     @Test
+    public void testVF2WithGraphDB100() throws IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        String queryGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\graphDB-100\\Q24.txt";
+        String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\graphDB-100\\dataset.txt";
+
+        MatchHelper.testIsomorphismAlgorithm(
+                targetGraphPath,
+                queryGraphPath,
+                VF2State.class,
+                new GraphDBDataSet(),
+                mapping -> false
+        );
+    }
+
+    @Test
     public void testVF2() throws IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\test\\isomorphism\\source_graph.txt";
         String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\test\\isomorphism\\target_graph.txt";
 
-        TestHelper.testIsomorphismAlgorithm(
+        MatchHelper.testIsomorphismAlgorithm(
                 targetGraphPath,
                 sourceGraphPath,
                 VF2State.class,
@@ -48,14 +62,54 @@ public class VF2Test {
 
     @Test
     public void testVF2WithEmailEuCore() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
-        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\Q4-10-unweighted.my";
+        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\Q4.txt-10-unweighted.my";
         String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\email-Eu-core.txt";
 
-        TestHelper.testIsomorphismAlgorithm(
+        MatchHelper.testIsomorphismAlgorithm(
                 targetGraphPath,
                 sourceGraphPath,
                 VF2State.class,
                 new UnweightedGraphDBDataSet(),
+                new EmailEuCoreDataSet(),
+                mapping -> {
+                    System.out.println(mapping.toString());
+                    return false;
+                }
+        );
+    }
+
+
+    @Test
+    public void testVF2WithEmailEuCoreAndQueryGraph() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
+//        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\unweighted-digraph\\V3E3.txt";
+//        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\unweighted-digraph\\V4E5.txt";
+//        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\unweighted-digraph\\V5E6.txt";
+        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\unweighted-digraph\\V6E7.txt";
+        String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\email-Eu-core.txt";
+
+        MatchHelper.testIsomorphismAlgorithm(
+                targetGraphPath,
+                sourceGraphPath,
+                VF2State.class,
+                new UnweightedDiGraphDataSet(),
+                new EmailEuCoreDataSet(),
+                mapping -> {
+                    System.out.println(mapping.toString());
+                    return false;
+                }
+        );
+    }
+
+    @Test
+    public void testVF2WithEmailEuCoreAndQueryGraph2() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
+        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\unweighted-digraph\\V7E7.txt";
+        String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\email-Eu-core.txt";
+
+        MatchHelper.testIsomorphismAlgorithm(
+                targetGraphPath,
+                sourceGraphPath,
+                VF2State.class,
+                new UnweightedDiGraphDataSet(),
                 new EmailEuCoreDataSet(),
                 mapping -> {
                     System.out.println(mapping.toString());
@@ -66,10 +120,10 @@ public class VF2Test {
 
     @Test
     public void testVF2WithEmailEuCoreDepartmentLabel() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
-        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\Q4-10-unweighted.my";
+        String sourceGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\Q4.txt-10-unweighted.my";
         String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\email-Eu-core\\email-Eu-core-department-labels.txt";
 
-        TestHelper.testIsomorphismAlgorithm(
+        MatchHelper.testIsomorphismAlgorithm(
                 targetGraphPath,
                 sourceGraphPath,
                 VF2State.class,
@@ -83,7 +137,7 @@ public class VF2Test {
     }
 
     @Test
-    public void testVF2WithUnweightedDiGraph()  throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
+    public void testVF2WithUnweightedDiGraph() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, IOException {
         // If you want to use the following data sets, you should run the unweighted-digraph-generator.py first.
 
         // python unweighted-digraph-generator.py multiple -v 5 -e 7 -n 5
@@ -91,7 +145,7 @@ public class VF2Test {
         // python unweighted-digraph-generator.py single -v 100 -e 1560
         String targetGraphPath = "F:\\IDEA\\subgraph-isomorphism\\datasets\\unweighted-digraph\\V100E1560.txt";
 
-        TestHelper.testIsomorphismAlgorithm(
+        MatchHelper.testIsomorphismAlgorithm(
                 targetGraphPath,
                 sourceGraphPath,
                 VF2State.class,
