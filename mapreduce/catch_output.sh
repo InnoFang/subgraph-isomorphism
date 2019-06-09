bb80f16af024bcc00adf84c2edf7d36959e7792a
InnoFang <innofang@outlook.com>
2019-06-09 14:31:01 +0800

    :art: udpate

--- mapreduce/catch_output.sh
+++ mapreduce/catch_output.sh
@@ -1,9 +1,9 @@
 echo '=== list all of the folder with the output content ==='
-hadoop fs -ls /output/subgraph-isomorphism/
+hadoop fs -ls hdfs://localhost:9000/user/innofang/output/
 echo '\n'
 read -p 'Which folder have the content you want to catch?' folder
 echo '=== list all of the file with the output content ==='
-hadoop fs -ls /output/subgraph-isomorphism/$folder
+hadoop fs -ls hdfs://localhost:9000/user/innofang/output/$folder
 echo '\n'
 read -p 'Which file have the content you want to catch?' file
-hadoop fs -text /output/subgraph-isomorphism/$folder/$file
+hadoop fs -text hdfs://localhost:9000/user/innofang/output/$folder/$file
